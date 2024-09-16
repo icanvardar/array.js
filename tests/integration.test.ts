ae74aaa3dc4045c21e312eb3c1e2e76f61abe139
Can Vardar <ismailcanvardar@gmail.com>
2024-09-16 15:01:12 +0300

    test(integration): add `ArrayList` test cases

---
+++ tests/integration.test.ts
@@ -0,0 +1,50 @@
+import { test, expect, mock } from "bun:test";
+import ArrayList from "../core/array-list";
+import type { TestType } from "./common";
+
+const createPrimitiveArrayList = <T extends SupportedPrimitives | SupportedNonPrimitives>(config?: ArrayListConfig) => {
+	return mock(() => {
+		return new ArrayList<T>(config);
+	});
+};
+
+test("test primitive type", () => {
+	const arrayList = createPrimitiveArrayList<number>({ dataType: "u8" })();
+
+	let items: number[] = [];
+	for (let i = 0; i <= 32; ++i) {
+		const randNum = Math.floor(Math.random() * 256);
+		arrayList.setData(i, randNum);
+		items.push(randNum);
+	}
+
+	for (let i = 0; i <= items.length; ++i) {
+		const data = arrayList.getData(i);
+		if (data) {
+			expect(data).toBe(items[i]);
+		}
+	}
+});
+
+test("test non-primitive type", () => {
+	const arrayList = createPrimitiveArrayList<TestType>()();
+
+	const testType: TestType = {
+		field1: 1,
+		field2: "test",
+		field3: {
+			field31: 1,
+			field32: true,
+		},
+	};
+
+	for (let i = 0; i <= 32; ++i) {
+		arrayList.setData(i, testType);
+	}
+
+	for (let i = 0; i <= 32; ++i) {
+		expect(arrayList.getData(i)).toBe(testType);
+	}
+});
+
+// TODO: add negative cases
