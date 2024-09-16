2198d0949554b06fa13e240c543643af5ba1379a
Can Vardar <ismailcanvardar@gmail.com>
2024-09-16 14:59:40 +0300

    chore: add `common.ts` for test files

---
+++ tests/common.ts
@@ -0,0 +1,17 @@
+export type TestType = {
+	field1: number,
+	field2: string,
+	field3: {
+		field31: number,
+		field32: boolean,
+	}
+}
+
+export interface TestInterface {
+	field1: number,
+	field2: string,
+	field3: {
+		field31: number,
+		field32: boolean,
+	}
+}
